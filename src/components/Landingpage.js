6050b2d1717aa4d4de61b07542811d57f5d563ca
ardi setyawan <ardisetyawan1621@gmail.com>
2020-01-16 01:08:38 +0700

    tes landing page

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -4,51 +4,53 @@ import { Grid, Cell } from 'react-mdl';
 class Landingpage extends Component{
     render(){
         return(
-           <div style={{width : '100%', margin: 'auto'}}>
-               <Grid className="landing-grid">
-                <Cell col={12}>
-                    <img
-                        src="https://www.w3schools.com/w3images/avatar2.png"
-                        alt="avatar"
-                        className="avatar-img"
-                    />
+        //    <div style={{width : '100%', margin: 'auto'}}>
+        //        <Grid className="landing-grid">
+        //         <Cell col={12}>
+        //             <img
+        //                 src="https://www.w3schools.com/w3images/avatar2.png"
+        //                 alt="avatar"
+        //                 className="avatar-img"
+        //             />
 
-                    <div className="banner-text">
-                        <h1>
-                            Front End Web Developer
-                        </h1>
-                        <hr/>
-                        <p> | HTML/CSS | Javascript | php | Git | GitHub | Bootstrap | React | Mysql |</p>
-                        <div className ="social-links">
-                            {/* LinkedIn */}
-                            <h2>
-                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-linkedin-square" aria-hidden="true"/>
-                                </a>
-                                {/* Github */}
-                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-github-square" aria-hidden="true"/>
-                                </a>
-                                {/* Freecodecamp */}
-                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-free-code-camp" aria-hidden="true"/>
-                                </a>
-                                {/* Youtube */}
-                                <a href="https://www.youtube.com/channel/UCPnSFMBx1e2iqiSkTFGEw1g" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-youtube-square" aria-hidden="true"/>
-                                </a>                            
-                                {/* Facebook */}
-                                <a href="https://web.facebook.com/ardi.setyawan.161?ref=bookmarks" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-facebook-square" aria-hidden="true"/>
-                                </a>
-                            </h2>
-                        </div>
+        //             <div className="banner-text">
+        //                 <h1>
+        //                     Front End Web Developer
+        //                 </h1>
+        //                 <hr/>
+        //                 <p> | HTML/CSS | Javascript | php | Git | GitHub | Bootstrap | React | Mysql |</p>
+        //                 <div className ="social-links">
+        //                     {/* LinkedIn */}
+        //                     <h2>
+        //                         <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+        //                         <i className="fa fa-linkedin-square" aria-hidden="true"/>
+        //                         </a>
+        //                         {/* Github */}
+        //                         <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+        //                         <i className="fa fa-github-square" aria-hidden="true"/>
+        //                         </a>
+        //                         {/* Freecodecamp */}
+        //                         <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+        //                         <i className="fa fa-free-code-camp" aria-hidden="true"/>
+        //                         </a>
+        //                         {/* Youtube */}
+        //                         <a href="https://www.youtube.com/channel/UCPnSFMBx1e2iqiSkTFGEw1g" rel="noopener noreferrer" target="_blank">
+        //                         <i className="fa fa-youtube-square" aria-hidden="true"/>
+        //                         </a>                            
+        //                         {/* Facebook */}
+        //                         <a href="https://web.facebook.com/ardi.setyawan.161?ref=bookmarks" rel="noopener noreferrer" target="_blank">
+        //                         <i className="fa fa-facebook-square" aria-hidden="true"/>
+        //                         </a>
+        //                     </h2>
+        //                 </div>
 
-                    </div>
+        //             </div>
 
-                </Cell>
-               </Grid>
-           </div>
+        //         </Cell>
+        //        </Grid>
+        //    </div>
+
+        console.log("hellow world")
         );
     }
 
