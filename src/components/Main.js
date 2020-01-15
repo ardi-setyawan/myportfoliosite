d02b4102af709be97a59c9639a55ef1f3a2dc90a
ardi setyawan <ardisetyawan1621@gmail.com>
2020-01-16 02:23:21 +0700

    coba main

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ import Project from './Project';
 
 const Main =() => (
     <HashRouter>
-        <Route exact path="/" component={Landingpage}/>
+        <Route exact path="/Landingpage" component={Landingpage}/>
         <Route path="/Aboutme" component={Aboutme}/>
         <Route path="/Contact" component={Contact}/>
         <Route path="/Project" component={Project}/>
