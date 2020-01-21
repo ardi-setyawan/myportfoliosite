44076fb86339992f1f9ac2e7aa3444d20c7d35fe
ardi setyawan <ardisetyawan1621@gmail.com>
2020-01-21 23:49:07 +0700

    meambahkan basename pada index.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,16 +6,16 @@ import Resume from './Resume';
 import Contact from './Contact';
 import Aboutme from './Aboutme';
 import Project from './Project';
+import { Switch } from 'react-mdl';
 
 const Main =() => (
-    <HashRouter>
+    <Switch>
         <Route exact path="/" component={Landingpage}/>
         <Route path="/Aboutme" component={Aboutme}/>
         <Route path="/Contact" component={Contact}/>
         <Route path="/Project" component={Project}/>
         <Route path="/Resume" component={Resume}/>
-    
-    </HashRouter>
+        </Switch>
 );
 
 
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename="myportfoliosite">
+        <Route exact path ="/" component={Exchange}>
+            </Route>
     <App />
     </BrowserRouter>
 , document.getElementById('root'));
