05a1dd92413b578215bd654da5b13720a0979ef4
ardi setyawan <ardisetyawan1621@gmail.com>
2020-01-21 23:00:28 +0700

    fuck

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch  , Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 
 import Landingpage from './Landingpage';
 import Resume from './Resume';
@@ -8,13 +9,13 @@ import Aboutme from './Aboutme';
 import Project from './Project';
 
 const Main =() => (
-    <Switch>
+    <HashRouter>
         <Route exact path="/" component={Landingpage}/>
         <Route path="/Aboutme" component={Aboutme}/>
         <Route path="/Contact" component={Contact}/>
         <Route path="/Project" component={Project}/>
         <Route path="/Resume" component={Resume}/>
-    </Switch>
+    </HashRouter>
 );
 
 
