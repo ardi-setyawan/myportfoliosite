7b9e85e573d08d4dfe905e78624fcff913051edf
ardi setyawan <ardisetyawan1621@gmail.com>
2020-01-22 01:13:23 +0700

    ks

--- src/index.js
+++ src/index.js
@@ -7,13 +7,12 @@ import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Landingpage from './components/Landingpage';
-import {BrowserRouter} from 'react-mdl';
 
 
 ReactDOM.render(
     <div>
     <BrowserRouter>
-        <Route exact path ="/" component={Landingpage}>
+        <Route exact path ="#" component={Landingpage}>
             </Route>
     <App />
     </BrowserRouter>
