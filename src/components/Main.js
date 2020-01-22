171a0df59abab3efc2b71b8c46136936bc631be2
ardi setyawan <ardisetyawan1621@gmail.com>
2020-01-22 20:03:14 +0700

    s

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { Route} from 'react-mdl';
+
 
 import Landingpage from './Landingpage';
 import Resume from './Resume';
