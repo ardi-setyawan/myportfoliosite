453e5c0948f53af8cbef8477d1cbdca25f90663e
ardi setyawan <ardisetyawan1621@gmail.com>
2020-01-22 20:24:16 +0700

    s

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Landingpage from './components/Landingpage';
-
+import { BrowserRouter, } from 'react-router-dom';
+import {Exchange} from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename="Landingpage">
+        <Route exact path="/"  component={Exchange} ></Route>
     <App />
     </BrowserRouter>
 , document.getElementById('root'));
