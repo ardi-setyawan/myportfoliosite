1e3a4d042d2be69febabad8ad47c36f79f40dcc5
ardi setyawan <ardisetyawan1621@gmail.com>
2020-01-29 00:10:46 +0700

    Updates

---
+++ precache-manifest.22e0664a84aa5e9b2f15ae0134b3799e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d690928d829b4d9e51650955e746c2a",
+    "url": "/myportfoliosite/index.html"
+  },
+  {
+    "revision": "c295fda4b2dd77f631c6",
+    "url": "/myportfoliosite/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "3ce230f3cb0b4322341e",
+    "url": "/myportfoliosite/static/css/main.1a8ec43f.chunk.css"
+  },
+  {
+    "revision": "c295fda4b2dd77f631c6",
+    "url": "/myportfoliosite/static/js/2.f2ab45a3.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/myportfoliosite/static/js/2.f2ab45a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ce230f3cb0b4322341e",
+    "url": "/myportfoliosite/static/js/main.6349ec64.chunk.js"
+  },
+  {
+    "revision": "fc7526bd7ede1edb81b7",
+    "url": "/myportfoliosite/static/js/runtime-main.81b18694.js"
+  }
+]);
